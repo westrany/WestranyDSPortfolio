2f5701400d8618bce54ce2ce0d94337e4c439e4b
Maria <maria.o.fitas@gmail.com>
2024-03-24 14:34:18 +0900

    Update WorkExperience.js
    Same issue as with the previous update. Now trying to render EXPERIENCES h1 outside of the 3 per row section of the code

--- src/containers/workExperience/WorkExperience.js
+++ src/containers/workExperience/WorkExperience.js
@@ -6,7 +6,7 @@ import {Fade} from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function WorkExperience() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
 
   if (workExperiences.display) {
     const renderExperienceRows = () => {
@@ -16,19 +16,19 @@ export default function WorkExperience() {
           <div className="experience-row" key={i}>
             {workExperiences.experience.slice(i, i + 3).map((card, index) => (
               <ExperienceCard
-                      key={index}
-                      isDark={isDark}
-                      cardInfo={{
-                        company: card.company,
-                        desc: card.desc,
-                        date: card.date,
-                        companylogo: card.companylogo,
-                        role: card.role,
-                        descBullets: card.descBullets
-                      }}
-                    />
-                  ))}
-            </div>
+                key={index}
+                isDark={isDark}
+                cardInfo={{
+                  company: card.company,
+                  desc: card.desc,
+                  date: card.date,
+                  companylogo: card.companylogo,
+                  role: card.role,
+                  descBullets: card.descBullets
+                }}
+              />
+            ))}
+          </div>
         );
       }
       return rows;
@@ -39,9 +39,11 @@ export default function WorkExperience() {
         <Fade bottom duration={1000} distance="20px">
           <div className="experience-container" id="workExperience">
             <h1 className="experience-heading">Experiences</h1> {/* Heading */}
-            {renderExperienceRows()} {/* Rows of experience cards */}
           </div>
         </Fade>
+        <div className="experience-container" id="workExperience">
+          {renderExperienceRows()} {/* Rows of experience cards */}
+        </div>
       </div>
     );
   }
